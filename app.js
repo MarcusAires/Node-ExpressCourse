4edd58a1bc9e3bc4c735380767abe12ec03112e5
MarcusAires <gadelhamarcus27@gmail.com>
2025-03-13 05:20:20 -0300

    criando servidor http com node puro

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const names = require('./4-names');
-const ola = require('./5-utils');
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        return res.end('Welcome to our home page');
+    }
+    if (req.url === '/about') {
+        return res.end('Here is our short history');
+    }
 
+    res.end(`
+        <h1>Ooops!</h1>
+        <p>We can't find the page you are looking for</p>
+        <a href="/">Back home</a>
+    `);
+});
 
-ola('marcus');
-ola(names.fulano);
-ola(names.ciclano);
-
+server.listen(5000, () => {
+    console.log('Server is running on port 5000...');
+});
