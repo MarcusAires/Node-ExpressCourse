32b87719e0e30a0be66c2e578f17f7eaebea96f9
marcusAires <gadelhamarcus27@gmail.com>
2025-03-14 05:37:54 -0300

    subindo

--- node-express-book/cap03/meadowlark.js
+++ node-express-book/cap03/meadowlark.js
@@ -1,26 +1,37 @@
 const express = require('express')
+const expressHandlebars = require('express-handlebars')
+const app = express()
 
-const app = express();
 
-const port = process.env.PORT || 3000
+app.use(express.static(__dirname + '/public'))
 
-// página 404 
+// configura o view engine Handlebars
+const hbs = expressHandlebars.create({
+  defaultLayout: 'main',
+})
+app.engine('handlebars', hbs.engine)  // Agora, passamos o engine diretamente
+app.set('view engine', 'handlebars')
 
-app.use((req,res)=>{
-    res.type('text/plain')
-    res.status(404)
-    res.send('404 - not found')
+const port = 3000 // Defina a variável 'port'
+
+// Definindo as rotas
+app.get('/', (req, res) => res.render('home'))
+app.get('/about', (req, res) => res.render('about'))
+
+// página 404 personalizada
+app.use((req, res) => {
+  res.status(404)
+  res.render('404')
 })
 
-//página 500
-
-app.use((err,req,res,next)=>{
-    console.error(err.message)
-    res.type('text/plain')
-    res.status(500)
-    res.send('500 - server error')
+// página 500 personalizada
+app.use((err, req, res, next) => {
+  console.error(err.message)
+  res.status(500)
+  res.render('500')
 })
 
-app.listen(port,()=>console.log(
-        `Express started on http://localhost:${port};`
-        + `press Ctrl-C to terminate.`))
+// Iniciando o servidor na porta 3000
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`)
+})
